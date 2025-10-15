df26a4f4d7a1b5215d055b5a641451933798eadc
Leandro Solovjovas dos Santos <solovjovas.leandro@gmail.com>
2025-10-15 11:39:32 +0000

    Adicionei o exercício ascii no repositório, onde trabalhei mais um pouco com prinf e scanf e tabela ascii

---
+++ fundamentos/ascii.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    char ascii;
+
+    printf("Digite um caractere: ");
+    scanf("%c", &ascii);
+    
+    printf("Caractere: %c\n", ascii);
+    printf("Ascii: %d\n", ascii);
+
+    return 0;
+    
+}
